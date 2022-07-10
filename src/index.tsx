67f1e94c63f1a32a660cb63acd8862360baa625d
Harry Hopkinson <63599884+Harry-Hopkinson@users.noreply.github.com>
2022-07-10 11:08:37 +0000

    Migrated to React Native

--- src/index.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import Barcode from "./Barcode";
-
-const root = ReactDOM.createRoot(
-  document.getElementById("root") as HTMLElement,
-);
-
-root.render(<Barcode />);
